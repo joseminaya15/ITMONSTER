8a568662ad5676fdea82405a26a231b0931c9d3f
jose minaya <joseminaya15@hotmail.com>
2019-09-19 06:46:09 -0500

    cambios

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8085/ITMONSTER';
+$config['base_url'] = 'http://localhost:8080/ITMONSTER';
 
 /*
 |--------------------------------------------------------------------------
